5bf7deac8cc040160b419c5164e9b1bf976b16e9
codingtech <codingtech390@gmail.com>
2024-10-05 03:29:07 +0530

    Completed first header as userCards

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
-  )
-}
+    <div className="flex flex-col md:flex-row p-4 gap-4">
+      {/* Left side */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent " />
+          <UserCard type="staff" />
+        </div>
+      </div>
+      {/* Right side */}
+      <div className="w-full lg:w-1/3">Right Side</div>
+    </div>
+  );
+};
 
-export default AdminPage
+export default AdminPage;
