91b72539ecd6175e87fa177a431327be9cbe5855
codingtech <codingtech390@gmail.com>
2024-10-05 20:23:26 +0530

    Completed UI for admin page. Also added role based filtering for sidebar data.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,15 +13,15 @@ export default function DashboardLayout({
     <div className="h-screen flex">
       {/* Left side*/}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-2.5">
-        <Link 
-          href="/" 
-          className="flex items-start justify-center lg:justify-start gap-2 p-4" >
+        <Link
+          href="/"
+          className="flex items-start justify-center lg:justify-start gap-2 p-4"
+        >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block lg:font-semibold">Academix</span>
         </Link>
 
         <Menu />
-
       </div>
       {/* Right side*/}
       <div className="w-[86%] md:w[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -113,6 +113,7 @@ const menuItems = [
   },
 ];
 
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -125,16 +126,20 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-4">
             {i.title}
           </span>
-          {i.items.map((items) => (
-            <Link
-              href={items.href}
-              key={items.label}
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={items.icon} alt="icon" width={20} height={20} />
-              <span className="hidden lg:block">{items.label}</span>
-            </Link>
-          ))}
+          {i.items.map((item) => {
+            if (item.visible.includes(role)) {
+              return (
+                <Link
+                  href={item.href}
+                  key={item.label}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded hover:bg-lamaSkyLight"
+                >
+                  <Image src={item.icon} alt="icon" width={20} height={20} />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              );
+            }
+          })}
         </div>
       ))}
     </div>
